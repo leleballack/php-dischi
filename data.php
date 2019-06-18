e318548b7b614eaeb4c5804e07a564a5a8b1f92c
leleballack <emanuele.ballacci@gmail.com>
2019-06-18 19:14:03 +0200

    Array Albums

---
+++ data.php
@@ -0,0 +1,65 @@
+<?php
+
+$albums = [
+  [
+    "pic" => "https://images-na.ssl-images-amazon.com/images/I/61hw9WloObL._SY355_.jpg"
+    "title" => "Dark Side of the Moon",
+    "artist" => "Pink Floyd",
+    "year" => "1973"
+  ],
+  [
+    "pic" => "https://altopedia.com/images/0/01/Joy-division-unknown-pleasures.jpg"
+    "title" => "Unknown Pleasures",
+    "artist" => "Joy Division",
+    "year" => "1979"
+  ],
+  [
+    "pic" => "https://images-na.ssl-images-amazon.com/images/I/816LHcgjwZL._SL1400_.jpg"
+    "title" => "War",
+    "artist" => "U2",
+    "year" => "1983"
+  ],
+  [
+    "pic" => "https://images-na.ssl-images-amazon.com/images/I/81irshLz8DL._SL1446_.jpg"
+    "title" => "Definitely Maybe",
+    "artist" => "Oasis",
+    "year" => "1994"
+  ],
+  [
+    "pic" => "https://upload.wikimedia.org/wikipedia/en/4/4c/Never_Mind_the_Bollocks%2C_Here%27s_the_Sex_Pistols.png"
+    "title" => "Never Mind the Bollocks, Here's the Sex Pistols",
+    "artist" => "Sex Pistols",
+    "year" => "1977"
+  ],
+  [
+    "pic" => "https://upload.wikimedia.org/wikipedia/en/0/00/TheClashLondonCallingalbumcover.jpg"
+    "title" => "London Calling",
+    "artist" => "The Clash",
+    "year" => "1979"
+  ],
+  [
+    "pic" => "https://metalitalia.com/wp-content/uploads/2018/11/Queen-Queen-II-1974.png"
+    "title" => "Queen II",
+    "artist" => "Queen",
+    "year" => "1974"
+  ],
+  [
+    "pic" => "https://static.lafeltrinelli.it/static/images-3/xl/457/8864457.jpg"
+    "title" => "Appetite for Destruction",
+    "artist" => "Guns N' Roses",
+    "year" => "1987"
+  ],
+  [
+    "pic" => "https://upload.wikimedia.org/wikipedia/en/2/26/Led_Zeppelin_-_Led_Zeppelin_IV.jpg"
+    "title" => "Led Zeppelin IV",
+    "artist" => "Led Zeppelin",
+    "year" => "1971"
+  ],
+  [
+    "pic" => "https://images-na.ssl-images-amazon.com/images/I/51O1R0wj00L._PJautoripBadge,BottomRight,4,-40_OU11__.jpg"
+    "title" => "In Utero",
+    "artist" => "Nirvana",
+    "year" => "1993"
+  ]
+];
+?>
--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+Stampare a schermo una decina di dischi musicali in due modi diversi:
+1 - Solo con l’utilizzo di PHP, che stampa direttamente i dischi in pagina: al caricamento della pagina ci saranno tutti i dischi.
+2 - Attraverso l’utilizzo di AJAX: al caricamento della pagina ajax chiederà attraverso una chiamata i dischi a php e li stamperà attraverso handlebars.
+Utilizzare: Html, Sass, JS, jQuery, handlebars, Php
+
 <?php
 include "data.php";
 ?>
